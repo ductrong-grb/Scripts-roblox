00c52bd7ceb0da7890d256e3fa0110a340100e29
ductrong-grb <ductrongvn3e@gmail.com>
2026-07-30 13:57:36 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@
 -- =================================================================
 
 -- CHÚ Ý: Hãy thay thế toàn bộ link ví dụ dưới đây bằng link RAW thật trên GitHub của bạn!
-local AIMBOT_URL = "https://raw.githubusercontent.com/TenGitHubCuaBan/TenRepo/main/aimbot.lua"
+local AIMBOT_URL = "https://raw.githubusercontent.com/ductrong-grb/Scripts-roblox/refs/heads/main/aimbot.lua"
 local ESP_URL    = "https://raw.githubusercontent.com/TenGitHubCuaBan/TenRepo/main/esp.lua"
 local UTILS_URL  = "https://raw.githubusercontent.com/TenGitHubCuaBan/TenRepo/main/utilities.lua"
 local UI_URL     = "https://raw.githubusercontent.com/TenGitHubCuaBan/TenRepo/main/ui.lua"
